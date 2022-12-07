4feda25ab521d5706e8447cb3d13e5eb43c3b69e
Dmitriy <dmitriy.shehobalov@gmail.com>
2022-12-07 08:36:58 +0300

    add task58

---
+++ task58/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 58: Задайте две матрицы. 
+// 1.Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (j < matr.GetLength(1) - 1) Console.Write($"{matr[i, j],4} |");
+            else Console.Write($"{matr[i, j],4}");
+        }
+        Console.WriteLine(" ]");
+    }
+}
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+int[,] MatrixMultiplication(int[,] matrixA, int[,] matrixB)
+{
+    if (matrixA.GetLength(1) != matrixB.GetLength(0))
+    {
+        System.Console.WriteLine("Multiplication is impossible!") ;
+    }
+    
+    int[,] matrixC = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+    for (int i = 0; i < matrixA.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixB.GetLength(1); j++)
+        {
+            matrixC[i, j] = 0;
+            for (int k = 0; k < matrixA.GetLength(1); k++)
+            {
+                matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+            }
+        }
+    }
+    return matrixC;
+}
+
+int[,] matrix = CreateMatrixRndInt(4, 4, 1, 9);
+PrintMatrix(matrix);
+Console.WriteLine();
+
+int[,] matrix2 = CreateMatrixRndInt(4, 4, 1, 9);
+PrintMatrix(matrix2);
+Console.WriteLine();
+
+int[,] matrixMultiplication = MatrixMultiplication(matrix, matrix2);
+PrintMatrix(matrixMultiplication);
